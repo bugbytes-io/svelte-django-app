5ec7100802ddfc57101fc71f6e0d5593d76e0032
bugbytes-io <bugbytes.io@gmail.com>
2023-10-11 19:18:20 +0100

    Video 2 code

--- Video2/src/routes/films/+page.svelte
+++ Video2/src/routes/films/+page.svelte
@@ -1,11 +1,36 @@
 <script>
     import {FilmStore} from '../../film-store'
+
+    let handleClick = () => FilmStore.update(prev => {
+        let newFilm = {id: 3, name: 'Drive', director: 'Nicolas Winding Refn'}
+        return [...prev, newFilm]
+    })
 </script>
 
-<ul>
-    {#each $FilmStore as film}
-        <li>
-            <a href="films/{film.id}">{film.name}</a>
-        </li>
-    {/each}
-</ul>
+<div>
+    <h2 class="my-4">Film List</h2>
+    
+    <div class="my-4 row">
+        {#each $FilmStore as film}
+        <div class="col-12 col-sm-6 col-md-4">
+            
+            <div class="card w-100 h-100">
+                <img class="card-img-top" style="height: 300px; object-fit: cover" 
+                    src="" 
+                    alt="Film">
+                <div class="card-body d-flex flex-column justify-content-between gap-4">
+					<div>
+                        <h5 class="card-title">{ film.name }</h5>
+                        <p class="card-text">Directed by { film.director }</p>
+                    </div>
+                    <div>
+                        <a href="/films/{film.id}" class="btn btn-primary">View</a>
+					</div>
+                </div>
+              </div>
+
+        </div>
+        {/each}
+    </div>
+    
+</div>
---
+++ Video2/src/routes/films/add/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import {FilmStore} from '../../../film-store'
+    import {goto} from '$app/navigation';
+    
+    let name = '';
+    let director = '';
+
+    let handleSubmit = () => {
+        FilmStore.update(prev => {
+            let id = prev[prev.length-1].id + 1
+            let newFilm = {id: id, name: name, director: director}
+            return [...prev, newFilm]
+        })
+
+        goto('/films/')
+    }
+</script>
+
+
+<div>
+
+    <h2 class="my-4">Add a Film</h2>
+
+    <div class="col-12 col-md-6">
+        <form on:submit|preventDefault={handleSubmit}>
+            <div class="mb-3">
+                <input class="form-control" type="text" placeholder="name" bind:value={name}/>
+            </div>
+            <div class="mb-3">
+                <input class="form-control" type="text" placeholder="director" bind:value={director}/>
+            </div>
+        
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </form>
+    </div>
+
+</div>
